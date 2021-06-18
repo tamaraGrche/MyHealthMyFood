d7703fbce9a8c33edd80fb6e84ac84dd7ce477d5
Tamara Grche <tamara.grche.98@gmail.com>
2021-06-18 22:58:42 +0200

    Create custom cell file named HomeTableViewCell. Connect UITableView to HomeViewController and setup Table View DataSource.

---
+++ MyHealthMyFood/MyHealthMyFood/App/Home/HomeTableViewCell.swift
@@ -0,0 +1,8 @@
+import Foundation
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
--- MyHealthMyFood/MyHealthMyFood/App/Home/HomeViewController.swift
+++ MyHealthMyFood/MyHealthMyFood/App/Home/HomeViewController.swift
@@ -1,13 +1,41 @@
 import Foundation
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigation()
+        setupTableView()
+    }
+    
+    // MARK: - Private Methods
+    private func setupNavigation() {
         navigationItem.title = "My Health My Food 🍓"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK: - Table View DataSource -
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell", for: indexPath) as? HomeTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.titleLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        cell.recipeImageView.image = UIImage(named: "launchLogo")
+        return cell
     }
 }
