1e11de0d86c9a38edeeecbc69c9fa9fe3e8c74fb
Tamara Grche <tamara.grche.98@gmail.com>
2021-06-23 00:38:20 +0200

    Add API URL recipes query.

--- MyHealthMyFood/MyHealthMyFood/App/API.swift
+++ MyHealthMyFood/MyHealthMyFood/App/API.swift
@@ -4,10 +4,11 @@ import Foundation
 struct API {
     
     struct URL {
-        static let test = "https://api.spoonacular.com/recipes/complexSearch\(key)"
+        static let test = "https://api.spoonacular.com/recipes/complexSearch?\(key)"
+        static let recipes = "https://api.spoonacular.com/recipes/complexSearch?sort=calories&sortDirection=asc&minFat=20&minProtein=10&maxProtein=100&number=30&\(key)"
     }
     
-    static let key = "?apiKey=1de5147fedbd4f7cb35a81418d400f97"
+    static let key = "apiKey=1de5147fedbd4f7cb35a81418d400f97"
 }
 
 
