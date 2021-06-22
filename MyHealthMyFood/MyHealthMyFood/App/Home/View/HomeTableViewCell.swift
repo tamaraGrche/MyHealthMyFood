38f9dffdea77ac9cb8b65d122407bb51889b6611
Tamara Grche <tamara.grche.98@gmail.com>
2021-06-23 01:26:59 +0200

    Add calories,fat and protein labels and icons in HomeTableViewCell.

--- MyHealthMyFood/MyHealthMyFood/App/Home/View/HomeTableViewCell.swift
+++ MyHealthMyFood/MyHealthMyFood/App/Home/View/HomeTableViewCell.swift
@@ -5,4 +5,7 @@ class HomeTableViewCell: UITableViewCell {
     
     @IBOutlet weak var recipeImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var fatLabel: UILabel!
+    @IBOutlet weak var proteinLabel: UILabel!
 }
