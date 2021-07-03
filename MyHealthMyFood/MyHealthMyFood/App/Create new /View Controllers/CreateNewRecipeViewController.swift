3da0d44315098b75bffb4e516fd5c987f4332b45
Tamara Grche <tamara.grche.98@gmail.com>
2021-07-03 21:34:10 +0200

    Check text fields and if they are empty show alert message.

--- MyHealthMyFood/MyHealthMyFood/App/Create new /View Controllers/CreateNewRecipeViewController.swift
+++ MyHealthMyFood/MyHealthMyFood/App/Create new /View Controllers/CreateNewRecipeViewController.swift
@@ -12,16 +12,52 @@ class CreateNewViewController: UIViewController {
     @IBOutlet weak var dietsTextField: UITextField!
     
    
-    // MARK: Life Cycle
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
     }
     
-    // MARK: IBAction
-    @IBAction func recipeImageButtonTapped(_ sender: Any) {
+    // MARK: - Private Methods
+    private func checkTextFields() {
+        guard let recipeTitle = recipeTitleTextField.text, !recipeTitle.isEmpty  else {
+           showAlert(title: "Missing field", message: "Recipe title is mendatory. Please insert title.")
+            return
+        }
+        
+        guard let healthScore = healthScoreTextField.text, !healthScore.isEmpty else {
+            showAlert(title: "Missing field", message: "Health score number is mendatory. Please insers number.")
+            return
+        }
+        
+        guard let time = timeTextField.text, !time.isEmpty else {
+            showAlert(title: "Missing field", message: "Recipe time for preparation is mendatory. Please insert your time.")
+            return
+        }
+        
+        guard let servingPeople = servingPeopleTextField.text, !servingPeople.isEmpty else {
+            showAlert(title: "Missing field", message: "Number of people for serving is mendatory. Please insert number of people.")
+            return
+        }
+        
+        guard let diets = dietsTextField.text, !diets.isEmpty else {
+            showAlert(title: "Missing field", message: "Diets for your recipe is mendatory. Please insert diets.")
+            return
+        }
+    
     }
     
+    private func showAlert(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Try again", style: .cancel, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
+    // MARK: - IBAction
+    @IBAction func recipeImageButtonTapped(_ sender: Any) {
+    }
+
+    
     @IBAction func saveButtonTapped(_ sender: Any) {
+        checkTextFields()
     }
 }
