a3c8198e46aa255765bb38538f6612876a18b86b
Tamara Grche <tamara.grche.98@gmail.com>
2021-07-03 20:25:22 +0200

    Connect IBOutlets to CreateNewRecipeViewController

---
+++ MyHealthMyFood/MyHealthMyFood/App/Create new /View Controllers/CreateNewRecipeViewController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+class CreateNewViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var recipeImageButton: UIButton!
+    @IBOutlet weak var recipeTitleTextField: UITextField!
+    @IBOutlet weak var healthScoreTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var servingPeopleTextField: UITextField!
+    @IBOutlet weak var dietsTextField: UITextField!
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: IBAction
+    @IBAction func recipeImageButtonTapped(_ sender: Any) {
+    }
+}
