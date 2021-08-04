f5c3cb934ff8e0cdda19d6739d3e33dfa06f5faf
Tamara Grche <tamara.grche.98@gmail.com>
2021-08-04 21:15:28 +0200

    Create ProfileViewController and connect it to storyboard.

---
+++ MyHealthMyFood/MyHealthMyFood/App/Profile/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
