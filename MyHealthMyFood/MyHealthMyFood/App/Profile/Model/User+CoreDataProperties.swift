5fa51a0ec7efd65109cdbb509bb27dd64eb81bc9
Tamara Grche <tamara.grche.98@gmail.com>
2021-08-04 21:08:36 +0200

    Create Core Data for the project. Add user as data model. Update AppDelegate with necessary methods for Core Data.

---
+++ MyHealthMyFood/MyHealthMyFood/App/Profile/Model/User+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+
+}
+
+extension User : Identifiable {
+
+}
